0e29d7289ab6455176c3db7851bf4ccd6c937609
juniormaximiano <juniormaximianoadeilson@hotmail.com>
2026-05-12 18:33:17 -0300

    feat: integrate task creation with frontend and backend

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -4,21 +4,49 @@ import type { Task } from "./types/Task";
 import { api } from "./services/api";
 
 function App() {
-
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   async function loadTasks() {
     const response = await api.get("/tasks");
     setTasks(response.data);
   }
 
+  async function createTask() {
+    if (title == "" || description == "") {
+      alert("Preencha todos os campos !");
+    } else {
+       await api.post("/tasks", { title, description });
+      setDescription("");
+      setTitle("");
+      loadTasks();
+    }
+  }
+
   useEffect(() => {
     loadTasks();
   }, []);
 
-    return (
+  return (
     <>
       <h1>Juno Log</h1>
+
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+        id="inputTitle"
+      />
+      <input
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        type="text"
+        id="inputDescription"
+      />
+      <button onClick={createTask} type="submit" id="btnSubmit">
+        Submit
+      </button>
 
       {tasks.map((task) => (
         <div key={task.id}>
